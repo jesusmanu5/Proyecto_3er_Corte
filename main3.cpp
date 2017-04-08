2b3cb463ab78a5a49fcaf4e2f22afc7c4306f5ae
Unknown <96jmanuel@gmail.com>
2017-04-08 17:25:01 -0430

    Agrege la funcion codificacion

--- main3.cpp
+++ main3.cpp
@@ -9,7 +9,28 @@ int n=0;
 int i=0;
 using namespace std;
 
- 
+void codifica(int n, string & cadena) 
+{ 
+    for (int i = 0; i < cadena.length(); i++) { 
+        if (cadena[i] >= 'a' && cadena[i] <= 'z') { 
+            if (cadena[i] + n > 'z') { 
+                cadena[i] = 'a' - 'z' + cadena[i] + n - 1; 
+            } else if (cadena[i] + n < 'a') { 
+                cadena[i] = 'z' - 'a' + cadena[i] + n + 1; 
+            } else { 
+                cadena[i] += n; 
+            } 
+        } else if (cadena[i] >= 'A' && cadena[i] <= 'Z') { 
+            if (cadena[i] + n > 'Z') { 
+                cadena[i] = 'A' - 'Z' + cadena[i] + n - 1; 
+            } else if (cadena[i] + n < 'A') { 
+                cadena[i] = 'Z' - 'A' + cadena[i] + n + 1; 
+            } else { 
+                cadena[i] += n; 
+            } 
+        } 
+    } 
+} 
 //Funcion descodificar 
 void descodifica(int n, string & cadena) 
 { 
@@ -19,6 +40,7 @@ void descodifica(int n, string & cadena)
 //Funcion main
 int main() 
 {
+    
     string cadena; 
     int n, op, tam, aux, cont = 0;
     char texto[50];
